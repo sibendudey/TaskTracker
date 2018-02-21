97b5bad7f09b40e6dcd72b7360bdd53963e300ab
Sibendu <sibendu.dey@gmail.com>
2018-02-20 20:56:29 -0500

    Simplified creation of Tasks

--- lib/tasktracker_web/controllers/task_controller.ex
+++ lib/tasktracker_web/controllers/task_controller.ex
@@ -6,10 +6,10 @@ defmodule TasktrackerWeb.TaskController do
   alias Tasktracker.TaskManager.Timetracker
   alias Tasktracker.Repo
 
-#  def index(conn, _params) do
-#    tasks = TaskManager.list_tasks()
-#    render(conn, "index.html", tasks: tasks)
-#  end
+  #  def index(conn, _params) do
+  #    tasks = TaskManager.list_tasks()
+  #    render(conn, "index.html", tasks: tasks)
+  #  end
 
   def new(conn, _params) do
     task = %Task{timetrackers: [%Timetracker{}]}
@@ -18,6 +18,52 @@ defmodule TasktrackerWeb.TaskController do
             |> Enum.map(&{&1.name, &1.id})
     render(conn, "new.html", changeset: changeset, users: users)
   end
+
+  #  def create(conn, %{"task" => task_params}) do
+  #
+  #    tasktracker_params = Map.get(task_params, "timetrackers")
+  #                         |> Map.get("0")
+  #                         |> Map.put("user_id", String.to_integer(Map.get(task_params, "user_id")))
+  #
+  #    tasktracker_params = for {key, val} <- tasktracker_params, into: %{}, do: {String.to_atom(key), val}
+  #
+  #    if tasktracker_params.user_id != get_session(conn, :user_id) do
+  #      tasktracker_params = tasktracker_params |> Map.delete(:time)
+  #    end
+  #
+  #    changeset = Timetracker.changeset(%Timetracker{}, tasktracker_params)
+  #
+  #    case changeset.valid? do
+  #      true -> case TaskManager.create_task(
+  #                     task_params
+  #                     |> Map.delete("timetrackers")
+  #                   ) do
+  #                {:ok, task} ->
+  #                  task_with_timetracker = Ecto.build_assoc(task, :timetrackers)
+  #                  case TaskManager.create_timetracker_with_post(task_with_timetracker, tasktracker_params) do
+  #                    {:ok, timetracker} ->
+  #                      conn
+  #                      |> put_flash(:info, "Task created successfully.")
+  #                      |> redirect(to: page_path(conn, :feed, user_id: get_session(conn, :user_id)))
+  #                    {:error, %Ecto.Changeset{} = changeset} ->
+  #                      users = Tasktracker.Accounts.list_users()
+  #                              |> Enum.map(&{&1.name, &1.id})
+  #                      render(conn, "new.html", changeset: changeset, users: users)
+  #                  end
+  #                {:error, %Ecto.Changeset{} = changeset} ->
+  #
+  #                  users = Tasktracker.Accounts.list_users()
+  #                          |> Enum.map(&{&1.name, &1.id})
+  #                  render(conn, "new.html", changeset: changeset, users: users)
+  #              end
+  #
+  #      false ->
+  #        changeset = Task.changesetWithTimetracker(%Task{}, task_params)
+  #        users = Tasktracker.Accounts.list_users()
+  #                |> Enum.map(&{&1.name, &1.id})
+  #        render(conn, "new.html", changeset: changeset, users: users)
+  #    end
+  #  end
 
   def create(conn, %{"task" => task_params}) do
 
@@ -28,41 +74,31 @@ defmodule TasktrackerWeb.TaskController do
     tasktracker_params = for {key, val} <- tasktracker_params, into: %{}, do: {String.to_atom(key), val}
 
     if tasktracker_params.user_id != get_session(conn, :user_id) do
-      tasktracker_params = tasktracker_params |> Map.delete(:time)
+      tasktracker_params = tasktracker_params
+                           |> Map.delete(:time)
     end
 
-    changeset = Timetracker.changeset(%Timetracker{}, tasktracker_params)
+    task_params = Map.put(task_params, "timetrackers", %{"0" => %{}} |> Map.put("0", tasktracker_params))
+    IO.inspect task_params
 
-    case changeset.valid? do
-      true -> case TaskManager.create_task(
-                     task_params
-                     |> Map.delete("timetrackers")
-                   ) do
-                {:ok, task} ->
-                  task_with_timetracker = Ecto.build_assoc(task, :timetrackers)
-                  case TaskManager.create_timetracker_with_post(task_with_timetracker, tasktracker_params) do
-                    {:ok, timetracker} ->
-                      conn
-                      |> put_flash(:info, "Task created successfully.")
-                      |> redirect(to: page_path(conn, :feed, user_id: get_session(conn, :user_id)))
-                    {:error, %Ecto.Changeset{} = changeset} ->
-                      users = Tasktracker.Accounts.list_users()
-                              |> Enum.map(&{&1.name, &1.id})
-                      render(conn, "new.html", changeset: changeset, users: users)
-                  end
-                {:error, %Ecto.Changeset{} = changeset} ->
-                  users = Tasktracker.Accounts.list_users()
-                          |> Enum.map(&{&1.name, &1.id})
-                  render(conn, "new.html", changeset: changeset, users: users)
-              end
-
-      false ->
-        changeset = Task.changesetWithTimetracker(%Task{}, task_params)
+    case TaskManager.create_task(task_params) do
+      {:ok, task} ->
+        conn
+        |> put_flash(:info, "Task created successfully.")
+        |> redirect(to: page_path(conn, :feed, user_id: get_session(conn, :user_id)))
+      {:error, %Ecto.Changeset{} = changeset} ->
         users = Tasktracker.Accounts.list_users()
                 |> Enum.map(&{&1.name, &1.id})
         render(conn, "new.html", changeset: changeset, users: users)
     end
+
+    #      false ->
+    #        changeset = Task.changesetWithTimetracker(%Task{}, task_params)
+    #        users = Tasktracker.Accounts.list_users()
+    #                |> Enum.map(&{&1.name, &1.id})
+    #        render(conn, "new.html", changeset: changeset, users: users)
   end
+
 
   def show(conn, %{"id" => id}) do
     task = TaskManager.get_task_with_single_timetracker!(id, get_session(conn, :user_id))
@@ -98,7 +134,11 @@ defmodule TasktrackerWeb.TaskController do
 
     case changeset.valid? do
       true ->
-        case TaskManager.update_task(task, task_params |> Map.delete("timetrackers")) do
+        case TaskManager.update_task(
+               task,
+               task_params
+               |> Map.delete("timetrackers")
+             ) do
           {:ok, task} ->
             timetracker = TaskManager.get_timetracker_by_post_id_and_user_id(id, Map.get(tasktracker_params, :user_id))
             if timetracker == nil do
@@ -117,7 +157,8 @@ defmodule TasktrackerWeb.TaskController do
         end
       false ->
         changeset = Task.changesetWithTimetracker(task, task_params)
-        users = Tasktracker.Accounts.list_users() |> Enum.map(&{&1.name, &1.id})
+        users = Tasktracker.Accounts.list_users()
+                |> Enum.map(&{&1.name, &1.id})
         render(conn, "edit.html", task: task, changeset: changeset, users: users)
     end
   end
